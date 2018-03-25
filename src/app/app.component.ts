28bcf879305789e83fbb3324bd173f581bdaee63
unknown <per@brf.dk>
2018-03-25 21:47:11 +0200

    Module 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+              <h2>{{ pageTitle }}</h2>
+              <div>My first component</div>
+            </div>`
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Github Browser';
 }
