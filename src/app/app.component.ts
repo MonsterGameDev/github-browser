fd66865723a5b2c453efaea1d79680001acdfb81
unknown <per@brf.dk>
2018-03-26 01:32:45 +0200

    Module 8 - routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,16 @@ import { RepoService } from './repo-list/repo.service';
   // tslint:disable-next-line:component-selector
   selector: 'app-root',
   template: `<div>
-              <h2>{{ pageTitle }}</h2>
-              <div>My first component</div>
-              <gh-repo-list></gh-repo-list>
+              <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                <a class='navbar-brand'>{{ pageTitle }}</a>
+                <ul class="nav navbar-nav">
+                  <li><a [routerLink]="['/home']">Home</a></li>
+                  <li><a [routerLink]="['/repos']">Repos</a></li>
+                </ul>
+                </div>
+                <router-outlet></router-outlet>
+              </nav>
             </div>`,
   providers: [RepoService]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http/';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { RepoListComponent } from './repo-list/repo-list.component';
 import { FormsModule } from '@angular/forms';
 import { StarComponent } from './shared/star.component/star.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 // import { RepoService } from './repo-list/repo.service';
 
 
@@ -15,12 +18,20 @@ import { StarComponent } from './shared/star.component/star.component';
   declarations: [
     AppComponent,
     RepoListComponent,
-    StarComponent
+    StarComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'repos', component: RepoListComponent},
+      {path: 'welcome', component: HomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   // providers: [RepoService],
   bootstrap: [AppComponent]
